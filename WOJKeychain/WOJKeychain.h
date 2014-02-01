13ef7854faeb41d4267b8a2c620f364423382fdd
Wojciech Jachowicz <wojciech.jachowicz@gmail.com>
2014-02-01 19:20:35 +0000

    Initial Commit

---
+++ WOJKeychain/WOJKeychain.h
@@ -0,0 +1,13 @@
+//
+//  WOJKeychain.h
+//  WOJKeychain
+//
+//  Created by Wojciech Jachowicz on 01.02.2014.
+//  Copyright (c) 2014 Wojciech Jachowicz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WOJKeychain : NSObject
+
+@end
---
+++ WOJKeychain/WOJKeychain.m
@@ -0,0 +1,13 @@
+//
+//  WOJKeychain.m
+//  WOJKeychain
+//
+//  Created by Wojciech Jachowicz on 01.02.2014.
+//  Copyright (c) 2014 Wojciech Jachowicz. All rights reserved.
+//
+
+#import "WOJKeychain.h"
+
+@implementation WOJKeychain
+
+@end
