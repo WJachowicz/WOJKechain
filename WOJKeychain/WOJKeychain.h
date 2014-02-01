e45ae365a3bbb0b90b832af81c5f07119dec9496
Wojciech Jachowicz <wojciech.jachowicz@gmail.com>
2014-02-01 20:08:52 +0000

    Implementation for saving and loading secure objects Cocoapods

--- WOJKeychain/WOJKeychain.h
+++ WOJKeychain/WOJKeychain.h
@@ -2,7 +2,7 @@
 //  WOJKeychain.h
 //  WOJKeychain
 //
-//  Created by Wojciech Jachowicz on 01.02.2014.
+//  Created by Wojciech Jachowicz on 27.01.2014.
 //  Copyright (c) 2014 Wojciech Jachowicz. All rights reserved.
 //
 
@@ -10,4 +10,31 @@
 
 @interface WOJKeychain : NSObject
 
+/**
+ *  A method for saving secure item in keychain.
+ *
+ *  @param object An object to be saved in keychain.
+ *  @param key    A search key which will be used for retrieving secure item.
+ *
+ *  @return BOOL value indicating whether the item was saved succefully or not.
+ */
++ (BOOL) saveSecureObject:(id) object forKey:(NSString *)key;
+
+/**
+ *  A method for load secure item from keychain.
+ *
+ *  @param key A search key for retrieving secure item.
+ *
+ *  @return Secure object or nil if the item was not found with provided key.
+ */
++ (id) loadSecureObjectForKey:(NSString *)key;
+
+/**
+ *  A method for deleting secure item from keychain under provided key.
+ *
+ *  @param key A search key for retrieving secure item.
+ *
+ *  @return BOOL value indicating whether the item was deleted or not.
+ */
++ (BOOL) deleteSecureObjectForKey:(NSString *)key;
 @end
--- WOJKeychain/WOJKeychain.m
+++ WOJKeychain/WOJKeychain.m
@@ -2,12 +2,78 @@
 //  WOJKeychain.m
 //  WOJKeychain
 //
-//  Created by Wojciech Jachowicz on 01.02.2014.
+//  Created by Wojciech Jachowicz on 27.01.2014.
 //  Copyright (c) 2014 Wojciech Jachowicz. All rights reserved.
 //
 
 #import "WOJKeychain.h"
+@import Security;
+
+@interface WOJKeychain ()
+
+/**
+ *  Method for creating dictionary containing a secure item class specification.
+ *
+ *  @param key NSString representing item search key.
+ *
+ *  @return Dictionary with secure item search specification.
+ */
++ (NSMutableDictionary *) secureQueryForKey:(NSString *)key;
+
+@end
 
 @implementation WOJKeychain
+
++ (BOOL)saveSecureObject:(id)object forKey:(NSString *)key
+{
+    
+    NSData *valueData = [NSKeyedArchiver archivedDataWithRootObject:object];
+    NSMutableDictionary * secureQuery = [self secureQueryForKey:key];
+    [secureQuery setObject:valueData forKey:(__bridge id)kSecValueData];
+    
+    CFTypeRef result = NULL;
+    OSStatus status = SecItemAdd((__bridge CFDictionaryRef)secureQuery, &result);
+    
+    if (status == errSecSuccess){
+        return YES;
+    } else {
+        return NO;
+    }
+}
+
++ (id)loadSecureObjectForKey:(NSString *)key
+{
+    NSMutableDictionary *secureQuery = [self secureQueryForKey:key];
+    [secureQuery setObject:(__bridge id)kCFBooleanTrue forKey:(__bridge id)kSecReturnData];
+    [secureQuery setObject:(__bridge id)kSecMatchLimitOne forKey:(__bridge id)kSecMatchLimit];
+    CFDataRef cfData = NULL;
+    OSStatus status = SecItemCopyMatching((__bridge CFDictionaryRef)secureQuery, (CFTypeRef *)&cfData);
+    if (status == errSecSuccess) {
+        id secureObject = [NSKeyedUnarchiver unarchiveObjectWithData:(__bridge_transfer NSData *)cfData];
+        CFRelease(cfData);
+        return secureObject;
+    } else{
+        if (cfData) {
+            CFRelease(cfData);
+        }
+        return nil;
+    }
+}
+
++ (BOOL)deleteSecureObjectForKey:(NSString *)key
+{
+    return NO;
+}
+
+#pragma mark - private
+
++ (NSMutableDictionary *)secureQueryForKey:(NSString *)key
+{
+    NSString *service = [[NSBundle mainBundle] bundleIdentifier];
+    if (!service) {
+        service = key;
+    }
+    return [NSMutableDictionary dictionaryWithObjects:@[(__bridge id)kSecClassGenericPassword, service, key] forKeys:@[(__bridge id)kSecClass, (__bridge id)kSecAttrService, (__bridge id)kSecAttrAccount]];
+}
 
 @end
